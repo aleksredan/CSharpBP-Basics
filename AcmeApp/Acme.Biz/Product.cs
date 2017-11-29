2dd385fee7aea4831b492744fa6f8831dcd8dfed
Aleksander Dominiczak <a.dominiczak@gmail.com>
2017-11-29 15:31:59 +0100

    Building Good Classes

---
+++ AcmeApp/Acme.Biz/Product.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    /// <summary>
+    /// Manages products carried in inventory.
+    /// </summary>
+    public class Product
+    {
+        public Product()
+        {
+            Console.WriteLine("Product instance created");
+        }
+
+        public Product(int productID, string productName, string description) : this()
+        {
+            this.ProductID = productID;
+            this.ProductName = productName;
+            this.Description = description;
+
+            Console.WriteLine("Product instance has a name: " + ProductName);
+        }
+
+        private string productName;
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+
+        private string description;
+
+        public string Description 
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        private int productID;
+
+        public int ProductID 
+        {
+            get { return productID; }
+            set { productID = value; }
+        }
+
+        public string SayHallo()
+        {
+            return "Hello " + ProductName + " (" + ProductID + "): " + Description;
+        }
+    }
+}
---
+++ AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        [TestMethod()]
+        public void SayHalloTest()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "Saw";
+            currentProduct.ProductID = 1;
+            currentProduct.Description = "15-inch steel blade hand saw";
+            var expected = "Hello Saw (1): 15-inch steel blade hand saw";
+
+            //Act
+            var actual = currentProduct.SayHallo();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SayHallo_ParametrizedConstructorTest()
+        {
+            //Arrange
+            var currentProduct = new Product(1, "Saw", "15-inch steel blade hand saw");
+            var expected = "Hello Saw (1): 15-inch steel blade hand saw";
+
+            //Act
+            var actual = currentProduct.SayHallo();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
